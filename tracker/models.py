ba6e4185a3c7173a515f856cd5c123a0e5dc0226
Gaurav Sharma <gauravsharma945413@gmail.com>
2025-07-18 22:05:41 +0530

    Add initial migration and implement models for CurrentBalance and TrackingHistory with view logic for expense tracking

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class CurrentBalance(models.Model):
+    current_balance = models.FloatField(default=0.0)
+
+class TrackingHistory(models.Model):
+    current_balance = models.ForeignKey(CurrentBalance, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    expense_type = models.CharField(choices=(('CREDIT', 'Credit'), ('DEBIT', 'Debit')), max_length=6)
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from .models import TrackingHistory, CurrentBalance
 # Create your views here.
 
 def index(request):
+    if request.method == 'POST':
+        description = request.POST.get('description')
+        amount = request.POST.get('amount')
+        current_balance, _ = CurrentBalance.objects.get_or_create(id = 1)
+        if float(amount) < 0:
+            expense_type = 'DEBIT'
+        else:
+            expense_type = 'CREDIT'
+        tracking_history = TrackingHistory.objects.create(
+            current_balance=current_balance,
+            amount=amount,
+            expense_type=expense_type,
+            description=description
+        )
+        current_balance.current_balance += float(tracking_history.amount)
+        current_balance.save()
+        print(f"Description: {description}, Amount: {amount}")
+        return redirect('/')
     return render(request, 'index.html')
