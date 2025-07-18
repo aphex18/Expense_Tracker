8959cf5c7fb83ee56e278ebcded32d9c7603f27a
Gaurav Sharma <gauravsharma945413@gmail.com>
2025-07-18 23:11:45 +0530

    Implement transaction deletion and add validation for zero amount in expense tracker

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render , redirect
 from .models import TrackingHistory, CurrentBalance
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -11,6 +12,9 @@ def index(request):
             expense_type = 'DEBIT'
         else:
             expense_type = 'CREDIT'
+        if float(amount) == 0:
+            messages.error(request, "Amount cannot be zero.")
+            return redirect('/')
         tracking_history = TrackingHistory.objects.create(
             current_balance=current_balance,
             amount=amount,
@@ -19,6 +23,29 @@ def index(request):
         )
         current_balance.current_balance += float(tracking_history.amount)
         current_balance.save()
-        print(f"Description: {description}, Amount: {amount}")
+        # print(f"Description: {description}, Amount: {amount}") # Debugging line to check form data coming or not
         return redirect('/')
-    return render(request, 'index.html')
+    income = 0
+    expense = 0
+    for transaction in TrackingHistory.objects.all():
+        if transaction.expense_type == 'CREDIT':
+            income += transaction.amount
+        else:
+            expense += transaction.amount
+    current_balance, _ = CurrentBalance.objects.get_or_create(id = 1)
+    context = {'transactions': TrackingHistory.objects.all(),
+               'current_balance' : current_balance,
+               'income': income,
+               'expense': expense
+               }
+    return render(request, 'index.html', context)
+
+def delete_transaction(request, id):
+    tracking_history = TrackingHistory.objects.filter(id = id)
+    if tracking_history.exists():
+            current_balance, _ = CurrentBalance.objects.get_or_create(id = 1)
+            tracking_history = tracking_history.first()
+            current_balance.current_balance -= tracking_history.amount
+            current_balance.save()
+    tracking_history.delete()
+    return redirect('/')
