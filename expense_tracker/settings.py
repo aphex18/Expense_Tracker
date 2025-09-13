a5ac93d269b897e3247070513e54c8e6d8ac7089
Gaurav Sharma <gauravsharma945413@gmail.com>
2025-09-13 20:20:29 +0530

    Fix DEBUG setting to use boolean value instead of string

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "False"
+DEBUG = False
 # DEBUG = True // true in development false in production
 ALLOWED_HOSTS = [ "expense-tracker-j6ud.onrender.com",
     "localhost","expense-tracker-j6ud.onrender.com"]  # Allow all hosts for development; change in production
