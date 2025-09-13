9bdce4352d075db6427d1291a380dac8cfb3762b
Gaurav Sharma <gauravsharma945413@gmail.com>
2025-09-13 21:09:54 +0530

    Update database port to default PostgreSQL port 5432

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT", "3306"),
+        'PORT': os.getenv("DB_PORT", "5432"),
     }
 
 }
