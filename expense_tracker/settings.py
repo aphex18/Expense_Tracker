7a7e33b77d215793302d58772a70ef1c507f3629
Gaurav Sharma <gauravsharma945413@gmail.com>
2025-09-13 21:06:54 +0530

    Change database engine from MySQL to PostgreSQL in settings

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'expense_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
