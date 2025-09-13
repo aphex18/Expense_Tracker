d73b4df4b4ab411523c613246812e607b95b06fb
Gaurav Sharma <gauravsharma945413@gmail.com>
2025-09-13 19:48:28 +0530

    Update DEBUG comment in settings.py for clarity and modify requirements.txt

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True" # Convert DEBUG to boolean
-
+# DEBUG = True // true in development false in production
 ALLOWED_HOSTS = ['*']  # Allow all hosts for development; change in production
 
 
