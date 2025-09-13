ee1a6a6545eaf730e4a75c17c463fc9b2da34ae1
Gaurav Sharma <gauravsharma945413@gmail.com>
2025-09-13 20:08:15 +0530

    Refactor DEBUG setting to use environment variable and update ALLOWED_HOSTS for production

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -26,9 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True" # Convert DEBUG to boolean
+DEBUG = "False"
 # DEBUG = True // true in development false in production
-ALLOWED_HOSTS = ['*']  # Allow all hosts for development; change in production
+ALLOWED_HOSTS = [ "expense-tracker-j6ud.onrender.com",
+    "localhost","expense-tracker-j6ud.onrender.com"]  # Allow all hosts for development; change in production
 
 
 # Application definition
